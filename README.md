dac921d9571e35dcb7ccec8db3f0f370083e8e96
Chris William <cb0806151@otc.edu>
2022-12-17 12:08:22 -0600

    complete draft of habit tracker v1

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # prayer-book-template
 
 Created by [Cendy](https://www.upwork.com/freelancers/~01b3ada479ef818cc7)
+
+Modified by Krysztof
+
+<details><summary>Description</summary>
+<p>  
+
+The draft of the SaintMaker PWA. Will eventually include a prayerbook, confession guide, habit tracker, Lectio Divina guide, and more.
+
+</p>
+</details>
+<details><summary>Development Log</summary>
+<p>
+
+12/10/22
+-make the header and drawer reusable between pages
+-start work on the habit tracker
+
+Backlog
+-wrap up draft of habit tracker
+-move habit data into indexdb
+-add prayer book and habits to hamburger
+-fix eslint linebreak system to work regardless of windows or linux
+-filter prayer html when implementing import/export functionality
+
+</p>
+</details>
--- src/pages/habits.tsx
+++ src/pages/habits.tsx
@@ -1,10 +1,120 @@
+import {Box, Button, ButtonGroup, Flex, IconButton, Input, Wrap} from '@chakra-ui/react';
+import {useRef, useState} from 'react';
+import {AiOutlinePlusCircle} from 'react-icons/ai';
+import {BsPencil} from 'react-icons/bs';
 import Header from '../components/Header';
 import Layout from '../components/Layout';
 
+type Habit = {
+  name: string
+  days: boolean[]
+  editing: boolean
+}
+
+const weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 
 function Habits() {
-  return (<Layout>
-    <Header title='Habits'><div>hello</div></Header></Layout>);
+  const [habits, setHabits] = useState<Habit[]>([]);
+  const addInputRef = useRef<HTMLInputElement | null>(null);
+  const editNameInputRef = useRef<HTMLInputElement | null>(null);
+  const addHabit = () => {
+    if (addInputRef?.current == undefined) return;
+    const newHabit = {
+      name: addInputRef.current.value,
+      days: [false, false, false, false, false, false, false],
+      editing: false,
+    };
+    if (newHabit.name.length === 0) return;
+    setHabits((state: Habit[]) => [...state, newHabit]);
+    addInputRef.current.value = '';
+  };
+  const deleteHabit = (deletedName: string) => {
+    const updatedHabits = habits.filter((habit) => habit.name !== deletedName);
+    setHabits(updatedHabits);
+  };
+  const updateLogic = (modifyHabit: (habit: Habit) => void) => {
+    const updatedHabits = habits.map((habit) => {
+      modifyHabit(habit);
+      return habit;
+    });
+    setHabits(updatedHabits);
+  };
+  const updateHabit = (oldHabitName: string, newHabitName: string) => {
+    if (newHabitName.length === 0) {
+      toggleEditing(oldHabitName);
+      return;
+    }
+    updateLogic((habit) => {
+      if (habit.name === oldHabitName) {
+        habit.name = newHabitName;
+        habit.editing = false;
+      }
+    });
+  };
+  const toggleHabitForDay = (habitName: string, dayIndex: number) => {
+    updateLogic((habit) => {
+      if (habit.name === habitName) {
+        habit.days[dayIndex] = !habit.days[dayIndex];
+      }
+    });
+  };
+  const toggleEditing = (habitName: string) => {
+    updateLogic((habit) => {
+      habit.editing = habit.name === habitName ? !habit.editing : false;
+    });
+  };
+
+  return (
+    <Layout>
+      <Header title='Habits'>
+        <Flex direction='row' pt='2'>
+          <Input placeholder='Add a new habit' ref={addInputRef} />
+          <IconButton
+            ml='2'
+            onClick={addHabit}
+            aria-label="Add prayer"
+            icon={<AiOutlinePlusCircle/>} />
+        </Flex>
+        {habits.map(({name, days, editing}, index) => (
+          <Box key={`${name}-${index}`} pt='2'>
+            <Box borderWidth='1px' borderRadius='lg' overflow='hidden' p='2'>
+              <Flex direction='column' alignItems='center'>
+                <Box>
+                  <Flex direction='row' justifyContent='space-between' pb='2'>
+                    <Box
+                      fontWeight='semibold'
+                      as='h4'
+                      lineHeight='tight'
+                      noOfLines={1}
+                    >
+                      {editing ? <Input type='text' placeholder='Edit habits current name' ref={editNameInputRef} /> : name}
+                    </Box>
+                    <IconButton
+                      colorScheme={editing ? 'green' : 'gray'}
+                      onClick={() => toggleEditing(name)}
+                      aria-label="Add prayer"
+                      icon={<BsPencil/>}/>
+                  </Flex>
+                  <Wrap spacing={2}>
+                    {weekdays.map((value, dayIndex) => (
+                    // TODO: figure out how to bring the logic from the colorScheme into a function without breaking the typing from chakra
+                      <IconButton colorScheme={days[dayIndex] ? 'green' : !days[dayIndex] && new Date().getDay() === dayIndex ? 'gray' : 'red'} key={`${name}-days-${dayIndex}`} onClick={() => toggleHabitForDay(name, dayIndex)} variant={days[dayIndex] ? 'solid' : 'outline'} aria-label='Add to friends' icon={<p>{value}</p>} />
+                    ))}
+                  </Wrap>
+                  {editing &&
+                <ButtonGroup pt='2'>
+                  <Button onClick={() => updateHabit(name, editNameInputRef.current !== null ? editNameInputRef.current.value : name)} colorScheme='green' variant='outline'>Update</Button>
+                  <Button onClick={() => deleteHabit(name)} colorScheme='red' variant='outline'>Delete</Button>
+                </ButtonGroup>
+                  }
+                </Box>
+              </Flex>
+            </Box>
+          </Box>
+        ))}
+      </Header>
+    </Layout>
+  );
 }
 
 export default Habits;
